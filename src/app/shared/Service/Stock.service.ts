0f417dafabb391f318b218519490edf0b655dfbe
Syrine Sassi <47872045+SyrineSassi@users.noreply.github.com>
2022-11-13 20:29:19 +0100

    Update Stock.service.ts

--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -12,7 +12,7 @@ export class StockService {
   constructor(private httpClient: HttpClient) { }
 
   getAllStocks() {
-    return this.httpClient.get(`http://192.168.1.14:8089/SpringMVC/stock/retrieve-all-stocks`)
+    return this.httpClient.get(`http://192.168.1.19:8089/SpringMVC/stock/retrieve-all-stocks`)
   }
   addStock(stock : any) {
     return this.httpClient.post(`${this.API_URL}/add-stock`, stock)
